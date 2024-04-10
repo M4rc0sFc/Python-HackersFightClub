14e04cce4b64a7b1a5fdf22de8780d4bcd9b4670
M4rc0sFc <91762429+M4rc0sFc@users.noreply.github.com>
2024-04-10 14:09:18 -0600

    Update Tarea4.py

--- Tareas/Tarea4.py
+++ Tareas/Tarea4.py
@@ -1,12 +1,17 @@
-#Script para dar un número aleatorio y pedir al usuario que adivine el número hasta que lo logre.
-import random 
+#función para generar numeros aleatorios
+def pseudo_random_number(i):
+    user_input = input("Please type anything and press Enter: ")
 
+    ascii_sum = sum(ord(c) for c in user_input)
 
-def revuelve_lista(lista):
-    for i in range(len(lista) - 1, 0, -1):
-        j = random.randint(0, i)  
-        lista[i], lista[j] = lista[j], lista[i]
-    return lista
+    random_numbers = []
+
+    for j in range(i + 1):  
+        pseudo_random = (ascii_sum * (j + 1)) % (i + 1)  # Example mathematical operation
+
+        random_numbers.append(pseudo_random)  
+
+    return random_numbers
 
 
 def comparacion(n,m):
@@ -15,18 +20,17 @@ def comparacion(n,m):
     else:
         return True
 
-lista_prueba = [i for i in range(99)]
-
-lista_revuelta = revuelve_lista(lista_prueba)
-print(lista_revuelta[41])
+lista_prueba = pseudo_random_number(99)
+#Imprime la lista generada y el número seleccionado a adivinar.
+#print(lista_prueba)
+#print(lista_prueba[41])
+print(lista_prueba[41])
 
 numero_adivinado = int(input("Adivina el número: "))
 
 while True:
-    if comparacion(lista_revuelta[41], numero_adivinado):
+    if comparacion(lista_prueba[41], numero_adivinado):
         print("Haz adivinado el número correcto")
         break 
     else:
         numero_adivinado = int(input("Adivina el número: "))
-        
-    
